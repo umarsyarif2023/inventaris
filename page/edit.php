417024dab072e4dcec202c030bc10fb72232005c
unknown <syarifumar295@gmail.com>
2023-08-09 07:07:50 +0700

    index.php

---
+++ page/edit.php
@@ -0,0 +1,48 @@
+<div class="row">
+    <h2>EDIT BARANG</h2>
+    <hr>
+    <div class="col-lg-4">
+    <div class="panel panel-info">
+        <div class="panel-heading">Tambah Barang Inventaris</div>
+            <div class="panel-body">
+            
+        <form action="form-group">
+            <div>
+            <label for="">Kode Barang</label>
+            <input type="text" class="form-control" placehoder="Masukkan Kode Barang">
+            </div>
+            <div class="form-group">
+            <label for="">Nama Barang</label>
+            <input type="text" class="form-control" placehoder="Masukkan Nama Barang">
+            </div>
+            <div class="form-group">
+            <label for="">Kondisi</label>
+            <select name="" id="" class="form-control">
+                <option value="" name="" class="form-control">Pilih Masbrow</option>
+                <option value="" name="" class="form-control">Rusak</option>
+                <option value="" name="" class="form-control">Baik</option>
+            </select>
+            </div>
+            <label for="">Jumlah</label>
+            <input type="number" class="form-control" placehoder="Masukkan Jumlah Barang">
+            <div class="form-group">
+            <label for="">Nama Ruang</label>
+            <input type="text" class="form-control" placehoder="Masukkan Nama Ruang">
+            </div>
+            <div class="form-group">
+            <label for="">Keterangan</label>
+            <textarea name="" id="" cols="10" rows="2" class="form-control"></textarea>
+            </div>
+            <button class="btn btn-md btn-primary">Save Bro</button>
+            <a href="?p=list_barang" class="btn btn-md btn-default">Kembali cuyy</a>
+            </div>
+        </form>
+    </div>
+</div>
+</div>              
+</div>
+    
+
+
+
+<!-- PUNYA UMAR -->
---
+++ page/list_barang.php
@@ -0,0 +1,70 @@
+<h2><center><b>DAFTAR INVENTARIS</b></center></h2>
+<h4><center><i>#umarsyarif</i></center></h4>
+
+<a href="?p=tambah_barang" class="table table-md btn-primay"><span class= "glyphicon glyphicon-plus"></span></a>
+<form class="navbar-form navbar-right" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Temukan Barang">
+        </div>
+        <button type="submit" class="btn btn-default">Gass cari</button>
+      </form>
+<br><br>
+
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Kode Inventaris</th>
+            <th>Nama Barang</th>
+            <th>Kondisi</th>
+            <th>Jumlah</th>
+            <th>Ruang</th>
+            <th>Tanggal register</th>
+            <th>Keterangan</th>
+            <th>Opsi</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>L001</td>
+            <td>Laptop</td>
+            <td>Aman</td>
+            <td>50</td>
+            <td>Gudang Umar</td>
+            <td>14-04-2008</td>
+            <td>Barang di dapat dari bantuan Provinsi</td>
+            <td>
+                <a href="?p=edit" class="btn btn-md btn-primary"><span class="glyphicon glyphicon-edit"></span></a>
+                <a href="" class="btn btn-md btn-danger"><span class="glyphicon glyphicon-trash"></span></a>
+            </td>
+
+        </tr>
+    </tbody>
+</table>
+
+<div class="float-left">
+    Jumlah=1
+
+</div>
+<div class="" style="float:right">
+<nav>
+  <ul class="pagination">
+    <li>
+      <a href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li><a href="#">1</a></li>
+    <li><a href="#">2</a></li>
+    <li><a href="#">3</a></li>
+    <li><a href="#">4</a></li>
+    <li><a href="#">5</a></li>
+    <li>
+      <a href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+</div>
